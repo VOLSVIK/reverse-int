c1bf47687984159ec3f4667c86a70d1ffad4b0db
volsvik <rudyard47@gmail.com>
2023-02-09 16:56:53 +0300

    feat: task completed

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function reverse (n) {
-  
+    n = ((Math.abs(n) + '').split(''))
+    n.reverse()
+    let i = 0
+    while (i < n.length) {
+        if (n[i] == '0') {
+            n.shift();
+            i = i - 1
+        } else {break}
+        i++
+      }
+    n = n.join('')
+    return n
 }
